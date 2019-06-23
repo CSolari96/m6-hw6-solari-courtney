551fdc6e4b56d232caaf883d0bf928e5282daea4
Courtney Solari <csolari@ufl.edu>
2019-06-22 22:50:58 -0500

    Create array of lunch choices in main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ placeholderSection.appendChild(randomLunchItem);
 // Create new empty text node and append to placeholder element
 var placeholderText = document.createTextNode("");
 randomLunchItem.appendChild(placeholderText);
+
+// Create array of Lunch Choices
+var lunchChoices = [
+	"Cheeseburger", 
+	"Hot Dog", 
+	"Fried Chicken", 
+	"Spagetti", 
+	"Ceasar Salad", 
+	"Fettuccine Alfredo", 
+	"Fish and Chips",
+	"Sub Sandwich",
+	"Tacos",
+	"Quesadilla",
+	"Pizza",
+	"Sushi"
+]
